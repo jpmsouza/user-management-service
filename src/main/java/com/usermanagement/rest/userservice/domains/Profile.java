152ac3387815b7279ca9e82adce33ccc51d8071e
João Pedro Martins Souza <joao.souza@gec.inatel.br>
2020-04-05 15:50:04 -0300

    Create relation between database tables and spring entities, database schema and initial loa

--- src/main/java/com/usermanagement/rest/userservice/domains/Profile.java
+++ src/main/java/com/usermanagement/rest/userservice/domains/Profile.java
@@ -1,5 +1,21 @@
 package com.usermanagement.rest.userservice.domains;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "PROFILE")
 public class Profile {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "NUM_ID")
+    private long id;
+
+    @Column(name = "STR_PROFILE")
+    private String profile;
 }
--- src/main/java/com/usermanagement/rest/userservice/domains/User.java
+++ src/main/java/com/usermanagement/rest/userservice/domains/User.java
@@ -1,5 +1,33 @@
 package com.usermanagement.rest.userservice.domains;
 
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "USER")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "NUM_ID")
+    private long id;
+
+    @Column(name = "STR_NAME")
+    private String name;
+
+    @Column(name = "STR_CPF")
+    private String cpf;
+
+    @Column(name = "STR_GENDER")
+    private String gender;
+
+    @Column(name = "DAT_BIRTH")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "STATUS")
+    private boolean status;
 }
