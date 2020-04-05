62c085cc6bc9b0a72f531e59c9332995c1866008
João Pedro Martins Souza <joao.souza@gec.inatel.br>
2020-04-05 17:31:23 -0300

    H2 setup
    It was necessary modify the file pom.xml to add some missing
    dependencies for junit test.
    At application.yml file, the user and password for h2 database was
    changed and change the server port.

--- src/main/java/com/usermanagement/rest/userservice/boot/UserserviceApplicationTests.java
+++ src/main/java/com/usermanagement/rest/userservice/boot/UserserviceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.usermanagement.rest.userservice.boot;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
